a28dca790ec4b605fd93f7956362a8076a0094d6
boyxiaolong <boyhailong@gmai.com>
2016-05-03 00:13:46 +0800

    save to json

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 import requests
 import bs4
+import json
 
-save_file = open("waimai_chaoren_data.txt", 'w')
+save_file = open("waimai_chaoren_json_data.txt", 'w')
 
 url = "http://waimaichaoren.com/restaurants/21886761/"
 resp = requests.get(url)
 soup = bs4.BeautifulSoup(resp.text)
 ##get all
+
 mydivs = soup.findAll("div", { "class" : "clearfix restaurant-food" })
 for div in mydivs:
     tmp = div.findAll('li')
     for tmpClass in tmp:
-        save_file.write(tmpClass.get_text()+"\n")
+        print(tmpClass.get_text())
 
 ##get current area restants name
 print("get current area restants name: ")
@@ -24,13 +26,19 @@ for div in mydivs:
         oneRestatantUrl = "http://waimaichaoren.com" + tmpClass.find('a', href=True).get('href')
         oneRest = requests.get(oneRestatantUrl)
         oneSoup = bs4.BeautifulSoup(oneRest.text).find_all("ul", {"class":"clearfix menu-group menu-group-img menu-first-load"})
-        save_file.write(tmpClass.get_text()+"\n")
+        tmpMap = {}
+        tmpMap["restrantName"] = tmpClass.get_text()
+        allFoods = []
         for oneSoupItem in oneSoup:
+            tmpOneMap = {}
             oneSoupItemName = oneSoupItem.find("div", {"class":"meun-item-name"})
             oneSoupItemPrice = oneSoupItem.find("span", {"class":"price"})
             oneSoupItemSellnum = oneSoupItem.find("span", {"class": "fr"})
-            save_str = "name: " + oneSoupItemName.get_text() +\
-            " price:" + oneSoupItemPrice.get_text() + " sellNum:" + oneSoupItemSellnum.get_text()\
-            + "\n"
-            save_file.write(save_str)
+            tmpOneMap["name"] = oneSoupItemName.get_text()
+            tmpOneMap["price"] = oneSoupItemPrice.get_text()
+            tmpOneMap["sellNum"] = oneSoupItemSellnum.get_text()
+            allFoods.append(tmpOneMap)
+        tmpMap["restrantFoods"] = allFoods
+        encodedjson = json.dumps(tmpMap)
+        save_file.write(encodedjson)
 save_file.close()
